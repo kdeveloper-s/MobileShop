37f19f41c515a016185a7f5e00102bf72e627456
kdeveloper-s <karlis.developer@gmail.com>
2021-08-15 19:42:42 +0300

    Create MobilePhone model

--- mobile_shop/shop/models.py
+++ mobile_shop/shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class MobilePhone(models.Model):
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    color1 = models.CharField(max_length=50)
+    color2 = models.CharField(max_length=50)
+    color3 = models.CharField(max_length=50)
+    camera_resolution = models.IntegerField()
