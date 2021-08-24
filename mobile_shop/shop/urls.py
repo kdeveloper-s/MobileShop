ef03a7879dd83032031294758987e7e110d6d5fc
Jazeps <jazeps.ivulis@gmail.com>
2021-08-25 01:16:02 +0300

    Fix templates, create cart funcionality

--- mobile_shop/shop/urls.py
+++ mobile_shop/shop/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('category/<slug:category_slug>/', views.products, name='products_by_category'),
-    path('category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
-    path('products/search/', views.search, name='search'),
+    path('products/<slug:category_slug>/', views.products, name='products_by_category'),
+    path('products/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('search/', views.search, name='search'),
     path('guide', views.guide, name='guide'),
     path('cart', views.cart, name='cart'),
+    path('add_cart/<int:product_id>', views.add_cart, name='add_cart'),
     path('register', views.register_request, name='register'),
     path('login', views.login_request, name='login'),
     path('logout', views.logout_request, name="logout"),
